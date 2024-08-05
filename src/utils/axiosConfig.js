1028e8bbbb35775e38122e3ed0b91c826b141ca5
Abdi <ab_cs2048@yahoo.com>
2024-08-05 09:36:28 +0300

    modified the package.json

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 export const base_url = "https://examprep-v33s.onrender.com/api/";
 
 const getTokenFromLocalStorage = () => {
-  const user = localStorage.getItem("user");
+  const user = localStorage.getItem('user');
   return user ? JSON.parse(user).token : null;
 };
 
